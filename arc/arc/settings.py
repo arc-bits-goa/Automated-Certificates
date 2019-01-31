ab29b88d10e6fd9568bf7ca3e8a5a0c405cee42e
Sarthak Agarwal <sarthakag12345@gmail.com>
2019-01-31 14:52:35 +0530

    minor changes to settings

--- arc/arc/settings.py
+++ arc/arc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=xqja@fzr5m$_q%eb$%m^li1p**j9=7)%mt)k=hn)$@@*^#%(4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','10.1.2.132:31415','localhost']
 
 
 # Application definition
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
